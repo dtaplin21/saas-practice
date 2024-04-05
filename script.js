b7802bdc2b4c7250d5ba8e8f64d4b5e84e5fffef
dominique Taplin <Dtaplin21@aol.com>
2024-04-04 19:03:25 -0700

    api key

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ const openaia = new OpenAIApi(new Configuration({
     apiKey: process.env.API_KEY
 }))
 
+openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: "Hello ChatGPT"}]
+}).then(res => {
+    console.log(res)
+})
